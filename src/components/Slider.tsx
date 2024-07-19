2cfa7cb4bb5442bfa3e26c5e65328c5ae3c03962
Sathish <sathish@alethea.in>
2024-07-19 15:52:31 +0530

    chore: Update sort-by npm dependency to version 0.0.2

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,75 +1,68 @@
-import { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-// import './ImageSlider.css';
 
 export default function ImageSlider() {
-  const slider1 = useRef(null);
-  const slider2 = useRef(null);
-
   const [nav1, setNav1] = useState<any>(null);
-  const [nav2, setNav2] = useState<any>(undefined);
+  const [nav2, setNav2] = useState<any>(null);
+  let sliderRef1 = useRef<any>(null);
+  let sliderRef2 = useRef<any>(null);
 
   useEffect(() => {
-    setNav1(slider1.current);
-    setNav2(slider2.current);
+    setNav1(sliderRef1);
+    setNav2(sliderRef2);
   }, []);
-
-  const handleSlideChange = (index: any) => {
-    if (nav1 && nav2) {
-      nav1.slickGoTo(index);
-      nav2.slickGoTo(index);
-    }
-  };
 
   return (
     <div className="slider-container">
+      <h2>Slider Syncing (AsNavFor)</h2>
       <h4>Second Slider</h4>
       <Slider
         asNavFor={nav1}
-        ref={slider2}
-        slidesToShow={9}
-        swipeToSlide={true}
+        ref={slider => (sliderRef2 = slider)}
+        slidesToShow={8.5}
         focusOnSelect={true}
-        centerMode={true} // Enable center mode
-        centerPadding="0" // Center the selected slide without additional padding
-        beforeChange={(_oldIndex: any, newIndex: any) => handleSlideChange(newIndex)}
+        centerMode={true}
+        centerPadding="10px"
       >
         <div>
-          <h3>1</h3>
+          <h4>Forms</h4>
         </div>
         <div>
           <h3>2</h3>
         </div>
         <div>
           <h3>3</h3>
+          <h4>Docs</h4>
         </div>
         <div>
           <h3>4</h3>
+          <h4>Time tracking</h4>
         </div>
         <div>
           <h3>5</h3>
+          <h4>Chat</h4>
         </div>
         <div>
           <h3>6</h3>
+          <h4>Whiteboards</h4>
         </div>
         <div>
           <h3>7</h3>
+          <h4>Projects</h4>
         </div>
         <div>
           <h3>8</h3>
+          <h4>Dashboard</h4>
         </div>
         <div>
           <h3>9</h3>
+          <h4>AI</h4>
         </div>
       </Slider>
       <h4>First Slider</h4>
-      <Slider
-        asNavFor={nav2}
-        ref={slider1}
-        beforeChange={(_oldIndex: any, newIndex: any) => handleSlideChange(newIndex)}
-      >
+      <Slider asNavFor={nav2} ref={slider => (sliderRef1 = slider)} className='main-banner'>
         <div>
           <h3><img src='/src/assets/sliders/1.png' /></h3>
         </div>
