1b5143802baf81f1839b7686ab50da11571ff692
bhadri01 <bhadri2002@gmail.com>
2024-07-19 13:51:14 +0530

    project setup ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import "./styles/app.scss"
+
 function App() {
   return (
-    <div className="bg-slate-900 text-slate-100 min-h-dvh flex justify-center items-center">welcome project</div>
+    <div className="title">welcome project</div>
   )
 }
 
