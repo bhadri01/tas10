2a1f49aa12254523a2297d160de713d47f054355
Sathish <sathish@alethea.in>
2024-07-19 16:59:01 +0530

    chore: Update max-width and margin for better alignment and spacing in CSS

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -15,86 +15,92 @@ export default function ImageSlider() {
   }, []);
 
   return (
-    <div className="slider-container">
-      <h2>Slider Syncing (AsNavFor)</h2>
-      <h4>Second Slider</h4>
-      <Slider
-        asNavFor={nav1}
-        ref={slider => (sliderRef2 = slider)}
-        slidesToShow={8.5}
-        focusOnSelect={true}
-        centerMode={true}
-        arrows={false}
-        centerPadding="0px"
-        className='nav-slider'
-      >
-        <div className='parent-box'>
-          <img src='/src/assets/sliders/dashboard-logo.svg' />
-          <h4 className='box-text'>Forms</h4>
-        </div>
-        <div className='parent-box'>
-          <img src='/src/assets/sliders/dashboard-logo.svg' />
-          <h4 className='box-text'>Sprints</h4>
-        </div>
-        <div className='parent-box'>
-          <img src='/src/assets/sliders/dashboard-logo.svg' />
-          <h4 className='box-text'>Docs</h4>
-        </div>
-        <div className='parent-box'>
-          <img src='/src/assets/sliders/dashboard-logo.svg' />
-          <h4 className='box-text'>Time tracking</h4>
-        </div>
-        <div className='parent-box'>
-          <img src='/src/assets/sliders/dashboard-logo.svg' />
-          <h4 className='box-text'>Chat</h4>
-        </div>
-        <div className='parent-box'>
-          <img src='/src/assets/sliders/dashboard-logo.svg' />
-          <h4 className='box-text'>Whiteboards</h4>
-        </div>
-        <div className='parent-box'>
-          <img src='/src/assets/sliders/dashboard-logo.svg' />
-          <h4 className='box-text'>Projects</h4>
-        </div>
-        <div className='parent-box'>
-          <img src='/src/assets/sliders/dashboard-logo.svg' />
-          <h4 className='box-text'>Dashboard</h4>
-        </div>
-        <div className='parent-box'>
-          <img src='/src/assets/sliders/dashboard-logo.svg' />
-          <h4 className='box-text'>AI</h4>
-        </div>
-      </Slider>
-      <Slider asNavFor={nav2} ref={slider => (sliderRef1 = slider)} className='main-banner'>
-        <div>
-          <h3><img src='/src/assets/sliders/1.png' /></h3>
-        </div>
-        <div>
-          <h3><img src='/src/assets/sliders/2.png' /></h3>
-        </div>
-        <div>
-          <h3><img src='/src/assets/sliders/3.png' /></h3>
-        </div>
-        <div>
-          <h3><img src='/src/assets/sliders/4.png' /></h3>
-        </div>
-        <div>
-          <h3><img src='/src/assets/sliders/5.png' /></h3>
-        </div>
-        <div>
-          <h3><img src='/src/assets/sliders/6.png' /></h3>
-        </div>
-        <div>
-          <h3><img src='/src/assets/sliders/7.png' /></h3>
-        </div>
-        <div>
-          <h3><img src='/src/assets/sliders/8.png' /></h3>
-        </div>
-        <div>
-          <h3><img src='/src/assets/sliders/9.png' /></h3>
-        </div>
-      </Slider>
+    <section className='slider-comp'>
+      <div className="slider-container">
+        <h2>Slider Syncing (AsNavFor)</h2>
+        <h4>Second Slider</h4>
+        <Slider
+          asNavFor={nav1}
+          ref={slider => (sliderRef2 = slider)}
+          slidesToShow={8.5}
+          focusOnSelect={true}
+          centerMode={true}
+          arrows={false}
+          centerPadding="0px"
+          className='nav-slider'
+        >
+          <div className='parent-box'>
+            <img src='/src/assets/sliders/dashboard-logo.svg' />
+            <h4 className='box-text'>Forms</h4>
+          </div>
+          <div className='parent-box'>
+            <img src='/src/assets/sliders/dashboard-logo.svg' />
+            <h4 className='box-text'>Sprints</h4>
+          </div>
+          <div className='parent-box'>
+            <img src='/src/assets/sliders/dashboard-logo.svg' />
+            <h4 className='box-text'>Docs</h4>
+          </div>
+          <div className='parent-box'>
+            <img src='/src/assets/sliders/dashboard-logo.svg' />
+            <h4 className='box-text'>Time tracking</h4>
+          </div>
+          <div className='parent-box'>
+            <img src='/src/assets/sliders/dashboard-logo.svg' />
+            <h4 className='box-text'>Chat</h4>
+          </div>
+          <div className='parent-box'>
+            <img src='/src/assets/sliders/dashboard-logo.svg' />
+            <h4 className='box-text'>Whiteboards</h4>
+          </div>
+          <div className='parent-box'>
+            <img src='/src/assets/sliders/dashboard-logo.svg' />
+            <h4 className='box-text'>Projects</h4>
+          </div>
+          <div className='parent-box'>
+            <img src='/src/assets/sliders/dashboard-logo.svg' />
+            <h4 className='box-text'>Dashboard</h4>
+          </div>
+          <div className='parent-box'>
+            <img src='/src/assets/sliders/dashboard-logo.svg' />
+            <h4 className='box-text'>AI</h4>
+          </div>
+        </Slider>
+        <Slider asNavFor={nav2} ref={slider => (sliderRef1 = slider)} className='main-banner'>
+          <div>
+            <h3><img src='/src/assets/sliders/1.png' /></h3>
+          </div>
+          <div>
+            <h3><img src='/src/assets/sliders/2.png' /></h3>
+          </div>
+          <div>
+            <h3><img src='/src/assets/sliders/3.png' /></h3>
+          </div>
+          <div>
+            <h3><img src='/src/assets/sliders/4.png' /></h3>
+          </div>
+          <div>
+            <h3><img src='/src/assets/sliders/5.png' /></h3>
+          </div>
+          <div>
+            <h3><img src='/src/assets/sliders/6.png' /></h3>
+          </div>
+          <div>
+            <h3><img src='/src/assets/sliders/7.png' /></h3>
+          </div>
+          <div>
+            <h3><img src='/src/assets/sliders/8.png' /></h3>
+          </div>
+          <div>
+            <h3><img src='/src/assets/sliders/9.png' /></h3>
+          </div>
+        </Slider>
 
-    </div>
+      </div>
+
+      <img src='/src/assets/background-highlighter1.svg' className='CuHomeTabs_svgLeft__NRC_E' />
+      <img src='/src/assets/background-highlighter2.svg' className='CuHomeTabs_svgRight__Cw476' />
+
+    </section>
   );
 }
