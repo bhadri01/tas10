28eead846670b74a0f5acb6fcf395af2acf50f9b
Sathish <sathish@alethea.in>
2024-07-20 23:17:34 +0530

    chore: Update Testimonial component responsive settings and styles

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -9,6 +9,32 @@ const Testimonial: React.FC = () => {
             <div className="slider">
                 <Slider swipeToSlide={true} touchMove={true} focusOnSelect={true} infinite={true} slidesToShow={3} slidesToScroll={1} autoplay={true} autoplaySpeed={2000} dots={true}
                     adaptiveHeight={true} waitForAnimate={true}
+                    responsive={[
+                        {
+                            breakpoint: 1024,
+                            settings: {
+                                slidesToShow: 3,
+                                slidesToScroll: 1,
+                                infinite: true,
+                                dots: true
+                            }
+                        },
+                        {
+                            breakpoint: 600,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
+                                initialSlide: 1
+                            }
+                        },
+                        {
+                            breakpoint: 480,
+                            settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 1
+                            }
+                        }
+                    ]}
                     arrows={false} speed={1000} className='test-slider'>
                     <div className='row'>
                         <div className="col-sm-4">
